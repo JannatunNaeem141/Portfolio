4524cff684bde8c60b0b60fe862a3e4e6866b2e1
jannatunnaeem141 <jannatunnaeem141@gmail.com>
2022-06-09 20:45:34 +0600

    navbar edited

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Footer from '../Shared/Navbar/Footer';
 import Navbar from '../Shared/Navbar/Navbar';
 
 const Home = () => {
     return (
         <div>
             <Navbar></Navbar>
+            <Footer></Footer>
         </div>
     );
 };
