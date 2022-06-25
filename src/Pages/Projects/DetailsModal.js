f4e92629947c2ea53849e90965303d61e60ed357
jannatunnaeem141 <jannatunnaeem141@gmail.com>
2022-06-25 15:54:40 +0600

    Details content updated in modal

--- src/Pages/Projects/DetailsModal.js
+++ src/Pages/Projects/DetailsModal.js
@@ -1,16 +1,46 @@
 import React from 'react';
 import './DetailsModal.css';
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/effect-flip";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { EffectFlip, Pagination, Navigation } from "swiper";
+import './Slider.css';
 
 const DetailsModal = ({ details }) => {
-    const { name, img1 } = details;
+    const { name, img1, img2, img3, img4, description } = details;
     return (
         <div>
             <input type="checkbox" id="details-modal" class="modal-toggle" />
             <div class="modal modal-bottom sm:modal-middle">
                 <div class="modal-box modal-bg">
                     <label for="details-modal" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+                    <>
+                        <Swiper
+                            effect={"flip"}
+                            grabCursor={true}
+                            pagination={true}
+                            navigation={true}
+                            modules={[EffectFlip, Pagination, Navigation]}
+                            className="mySwiper"
+                        >
+                            <SwiperSlide>
+                                <img src={img1} />
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <img src={img2} />
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <img src={img3} />
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <img src={img4} />
+                            </SwiperSlide>
+                        </Swiper>
+                    </>
                     <h3 class="font-bold text-lg modal-head text-secondary">{name}</h3>
-                    <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+                    <p class="py-4">{description}</p>
                 </div>
             </div>
         </div>
---
+++ src/Pages/Projects/Slider.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/effect-flip";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { EffectFlip, Pagination, Navigation } from "swiper";
+import './Slider.css';
+
+
+const Slider = () => {
+    return (
+        <>
+            <Swiper
+                effect={"flip"}
+                grabCursor={true}
+                pagination={true}
+                navigation={true}
+                modules={[EffectFlip, Pagination, Navigation]}
+                className="mySwiper"
+            >
+                <SwiperSlide>
+                    <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src="https://swiperjs.com/demos/images/nature-6.jpg" />
+                </SwiperSlide>
+            </Swiper>
+        </>
+    );
+};
+
+export default Slider;
